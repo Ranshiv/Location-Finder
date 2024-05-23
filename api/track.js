9e6cd03b5eb6178a0cdd720f47f1b46a27e4e082
Ranshiv Kumar <ranshiv369@gmail.com>
2024-05-23 02:10:44 -0400

    Third Commit

--- api/track.js
+++ api/track.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = async (req, res) => {
   const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -8,9 +10,14 @@ module.exports = async (req, res) => {
     const response = await axios.get(`https://ipapi.co/${userIp}/json/`);
     const location = response.data;
     console.log(`User Location: ${JSON.stringify(location)}`);
-    res.status(200).send(`Your IP address is ${userIp} and your location is ${location.city}, ${location.region}, ${location.country_name}`);
+    
+    const logMessage = `IP: ${userIp}, Location: ${location.city}, ${location.region}, ${location.country_name}\n`;
+    const logFilePath = path.join(__dirname, '..', 'logs', 'access.log');
+    fs.appendFileSync(logFilePath, logMessage, 'utf8');
+    
+    res.status(200).send(`Your request has been logged.`);
   } catch (error) {
     console.error(`Error fetching location for IP ${userIp}: ${error.message}`);
-    res.status(500).send(`Your IP address is ${userIp}. Unable to determine location.`);
+    res.status(500).send(`Unable to determine location. Your request has been logged.`);
   }
 };
